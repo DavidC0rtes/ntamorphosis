98736573d82fecf7b04c029628f1181f10effc4e
Santiago Cortes <david.cortes@correounivalle.edu.co>
2023-03-15 17:31:08 +0100

    Round robin execution

--- src/main/java/org/neocities/daviddev/ntamorphosis/entrypoint/Runner.java
+++ src/main/java/org/neocities/daviddev/ntamorphosis/entrypoint/Runner.java
@@ -4,7 +4,6 @@ import Parser.Main.EntryPoint;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.neocities.daviddev.bisimcheck.runners.BisimRunner;
-import org.neocities.daviddev.bisimcheck.workers.Bisimulation;
 import org.neocities.daviddev.ntamorphosis.pairprocessing.XMLFileProcessor;
 
 import java.io.File;
@@ -13,10 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,7 +26,7 @@ public class Runner {
     private HashMap<String, String[]> resultsBisim;
 
     private static enum tronHeaders {
-        model, mutant, test_passed, elapsed_time
+        mutant1, mutant2, template, passed_test, diff_locations, explored_diffs, elapsed_time
     }
 
     public Runner(File model, List<String> operators) {
@@ -49,7 +45,7 @@ public class Runner {
     
     public Runner(String dir) {
         this.mutationsDir = dir;
-        this.executorService = Executors.newSingleThreadExecutor();
+        this.executorService = Executors.newFixedThreadPool(4);
         resultsTron = new HashMap<>();
         prepareCSV();
         execSimmDiffRRSingles();
@@ -104,33 +100,6 @@ public class Runner {
         wrapUp(tronTask);
     }
 
-    private void execSimmDiffV2() {
-        String modelName = model.getName().split("\\.")[0];
-        Path dir = Paths.get(mutationsDir+"/"+modelName);
-        try (Stream<Path> entries = Files.walk(dir, 1)){
-            entries.forEach(mutant -> {
-                if (Files.isRegularFile(mutant) && !mutant.endsWith(".csv")) {
-
-                    Runnable tronTask = (() -> {
-                        //System.out.printf("Executing with mutant %s and model %s\n", mutant, model.getName());
-                        org.neocities.daviddev.simmdiff.entrypoint.Runner simRunner = new org.neocities.daviddev.simmdiff.entrypoint.Runner(
-                                model,
-                                mutant.toFile()
-                        );
-                        simRunner.parseModels();
-                        simRunner.parseTraces();
-                        simRunner.simulateTraces();
-                        resultsTron.putAll(simRunner.getTracesResult());
-                    });
-                    wrapUp(tronTask);
-                }
-            });
-        } catch (IOException e) {
-            System.err.println("working with model "+modelName);
-            throw new RuntimeException(e);
-        }
-    }
-
      /**
      * Executes SimmDiffUppaal with every generated mutant and its model, keeps a record
      * of mutants that failed the tron test.
@@ -148,15 +117,15 @@ public class Runner {
         File directory = new File(mutationsDir);
         File[] xmlFiles = directory.listFiles((dir, name) -> name.endsWith(".xml"));
 
-        for (int i = 0; i < xmlFiles.length - 1; i++) {
+        for (int i = 0; i < Objects.requireNonNull(xmlFiles).length - 1; i++) {
             for (int j = i + 1; j < xmlFiles.length; j++) {
                 File file1 = xmlFiles[i];
                 File file2 = xmlFiles[j];
-
                 XMLFileProcessor processor = new XMLFileProcessor();
-                Runnable tronTask = (() ->
-                        processor.runSimmDiff(file1, file2, resultsTron)
-                );
+                Runnable tronTask = (() -> {
+                    resultsTron.putAll(processor.runSimmDiff(file1, file2, resultsTron));
+                    resultsTron.putAll(processor.runSimmDiff(file2, file1, resultsTron));
+                });
 
                 wrapUp(tronTask);
             }
@@ -185,16 +154,17 @@ public class Runner {
         }
     }
     private synchronized void printCSV() {
-
         try (CSVPrinter printer = new CSVPrinter(new FileWriter("traces-result.csv", true), CSVFormat.DEFAULT)) {
 
             // Key: filename of mutant.
             // Value: String[]{path to model, tron result}
             for (var entry : resultsTron.entrySet()) {
                 Object[] result = entry.getValue();
-                printer.printRecord(entry.getKey(), result[0], result[1], result[2]);
+                System.out.println("### " + entry.getKey() + " " + Arrays.toString(result));
+                printer.printRecord(entry.getKey(), result[0], result[1], result[2],  result[3], result[4], result[5]);
             }
             printer.flush();
+            resultsTron.clear();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/neocities/daviddev/ntamorphosis/pairprocessing/XMLFileProcessor.java
+++ src/main/java/org/neocities/daviddev/ntamorphosis/pairprocessing/XMLFileProcessor.java
@@ -5,6 +5,7 @@ import org.neocities.daviddev.simmdiff.entrypoint.Runner;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class XMLFileProcessor {
 
@@ -12,12 +13,13 @@ public class XMLFileProcessor {
         File directory = new File(directoryPath);
         File[] xmlFiles = directory.listFiles((dir, name) -> name.endsWith(".xml"));
 
-        for (int i = 0; i < xmlFiles.length - 1; i++) {
+        for (int i = 0; i < Objects.requireNonNull(xmlFiles).length - 1; i++) {
             for (int j = i + 1; j < xmlFiles.length; j++) {
                 File file1 = xmlFiles[i];
                 File file2 = xmlFiles[j];
 
                 runSimmDiff(file1, file2, resultsTron);
+                runSimmDiff(file2, file1, resultsTron);
             }
         }
     }
@@ -27,23 +29,14 @@ public class XMLFileProcessor {
         runSimmDiff(file1, file2, resultsTron);
     }
 
-    public void runSimmDiff(File file1, File file2, HashMap<String, String[]> resultsTron) {
+    public HashMap<String, String[]> runSimmDiff(File file1, File file2, HashMap<String, String[]> resultsTron) {
+        System.out.printf("Simmdiff with %s and %s\n", file1.getName(), file2.getName());
         Runner runner1 = new Runner(file1, file2);
 
         runner1.parseModels();
         runner1.parseTraces();
         runner1.simulateTraces();
 
-
-        resultsTron.putAll(runner1.getTracesResult());
-
-        Runner runner2 = new Runner(file2, file1);
-
-        runner2.parseModels();
-        runner2.parseTraces();
-        runner2.simulateTraces();
-
-
-        resultsTron.putAll(runner2.getTracesResult());
+        return runner1.getTracesResult();
     }
 }
