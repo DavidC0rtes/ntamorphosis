a42c0eee9fbfdc8f46e07369d05990760f7b5992
Santiago Cortes <david.cortes@correounivalle.edu.co>
2023-06-19 18:54:28 +0200

    Clearer instructions for cli args.

--- src/main/java/org/neocities/daviddev/ntamorphosis/Main.java
+++ src/main/java/org/neocities/daviddev/ntamorphosis/Main.java
@@ -7,35 +7,34 @@ import picocli.CommandLine;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @Command(name = "NTAMorphosis", version = "0.1", mixinStandardHelpOptions = true)
 public class Main implements Runnable {
     enum STRATEGIES { random, biased }
-    @Option(names = {"-model"}, description = "Path to model file")
+    @Option(names = {"--model"}, description = "Path to model's file.")
     File model;
 
     @Option(names = {"-op", "--operators"})
     ArrayList<String> operators = new ArrayList<>();
 
-    @Option(names = {"-dir","--mutants-dir"}, description = "Path were the mutants are", defaultValue = "src/main/resources/mutations")
+    @Option(names = {"-dir","--mutants-dir"}, description = "Path to directory where the mutants are.", defaultValue = "src/main/resources/mutations")
     String outPath;
 
-    @Option(names = {"-csv","--csv-path"}, description = "Csv path", defaultValue = "traces-result.csv")
+    @Option(names = {"-csv","--csv-path"}, description = "Name and path to csv with TraceMatcher's result.", defaultValue = "traces-result.csv")
     String csvPath;
 
-    @Option(names = {"-csvb","--csv-bisim"}, description = "Csv path for bisim report", defaultValue = "results_bisim.csv")
+    @Option(names = {"-csvb","--csv-bisim"}, description = "Name and path to csv for bisimulation result.", defaultValue = "results-bisim.csv")
     String csvBisim;
 
-    @Option(names = {"-eq", "--equivalent"}, description = "Compute bisimulation w/ respect to the original model", defaultValue = "false")
+    @Option(names = {"-eq", "--equivalent"}, description = "Compute bisimulation w/ respect to the original model.", defaultValue = "false")
     boolean getEquivalent;
 
-    @Option(names = {"-dup", "--duplicates"}, description = "Compute bisimulation between each mutant", defaultValue = "false")
+    @Option(names = {"-dup", "--duplicates"}, description = "Compute bisimulation between each mutant.", defaultValue = "false")
     boolean getDuplicates;
 
     @Option(names = "--gui", description = "Use the gui. True by default.", defaultValue = "true", fallbackValue = "true", negatable = true)
     boolean gui;
-    @Option(names = "-how", description = "How to generate traces, one of: ${COMPLETION-CANDIDATES}", defaultValue = "biased")
+    @Option(names = "--how", description = "How to generate traces, one of: ${COMPLETION-CANDIDATES}", defaultValue = "biased")
     STRATEGIES strategy;
 
     public static void main(String[] args) {
